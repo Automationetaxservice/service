9b1c4c18a3f75a231b9a5bcfb1937b7bbfbff8fd
E Automation Software <eautomationdep@francistaxservice.com>
2024-07-09 14:16:37 -0500

    Updates

--- proxy.js
+++
@@ -1,7 +0,0 @@
-
-import ProxyFrameController from '@ringcentral-integration/widgets/lib/ProxyFrameController';
-import prefix from './prefix';
-
-export default new ProxyFrameController({
-  prefix,
-});
